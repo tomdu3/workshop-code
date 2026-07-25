e667e7b90ca3e051fc3b6714337a015a42c0e9f5
vmoratti <vmoratti@gmail.com>
2026-07-25 11:35:24 +0100

    add button

--- dom-merge-conflict/tasks/buttons-and-counter/src/app.js
+++ dom-merge-conflict/tasks/buttons-and-counter/src/app.js
@@ -11,6 +11,7 @@ export function App() {
   header.innerHTML = `
         <h1>Number Counter</h1>
         <p>A simple counter. Press increment to increase the count by one.</p>
+        <p>A simple counter. Press decrement to decrease the count by one.</p>
     `;
   body.appendChild(header);
 
@@ -18,10 +19,13 @@ export function App() {
   main.innerHTML = `
         <p id="counter" data-testid="counter">0</p>
         <button id="increment">Increment</button>
+        <button id="decrement">Decrement</button>
     `;
+
   body.appendChild(main);
 
   const button = body.querySelector("#increment");
+
   const counter = body.querySelector("#counter");
   button.addEventListener("click", () => {
     increment(counter);
